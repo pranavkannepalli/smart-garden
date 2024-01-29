a5349d822fa3fec9b5fe3a9ba46951f5ec4c68fa
PranavK226 <pranav.kannepalli@gmail.com>
2024-01-29 10:35:32 -0800

    deleted gauge

--- app/page.js
+++ app/page.js
@@ -1,7 +1,5 @@
-import GaugeChart from 'react-gauge-chart';
-
 async function getData() {
-  const res = await fetch('https://smart-garden-rho.vercel.app/plantdata', { next: { revalidate: 600 } })
+  const res = await fetch('https://smart-garden-rho.vercel.app/plantdata', { next: { revalidate: 60 } })
  
   if (!res.ok) {
     throw new Error('Failed to fetch data')
@@ -17,7 +15,9 @@ export default async function Page() {
   return (
     <main>
       <h1>Plant {currplant}</h1>
-      <div>{data[currplant]['soilTemp']}</div>
+      <p>
+        {data[0]['soilTemp']/100.0 ?? 0} 
+      </p>
     </main>
   )
 }
