18ad7b5446662798f58b3e73def1e336d264bdc8
PranavK226 <pranav.kannepalli@gmail.com>
2024-01-29 11:56:02 -0800

    created guages

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+import styles from './page.module.css';
+
 async function getData() {
   const res = await fetch('https://smart-garden-rho.vercel.app/plantdata', { next: { revalidate: 60 } })
  
@@ -11,13 +13,30 @@ async function getData() {
 export default async function Page() {
   const data = await getData();
   var currplant = 0;
+  const headers = {'temp':50.0, 'humidity':100.0, 'soilMoisture':100.0, 'pump':1, 'soilTemp':50.0, 'sunlight':100.0}
+
+  // temp: 0 - 50
+  // humidity : 0 - 100
+  // soilMoisture : 0 - 100
+  // pump : 0 - 1
+  // soilTemp: 0 - 50
+  // sunlight0 - 100
  
   return (
     <main>
-      <h1>Plant {currplant}</h1>
-      <p>
-        {data[0]['soilTemp']/100.0 ?? 0} 
-      </p>
+      <body>
+        <h1>Plant {currplant}</h1>
+        <div className={styles.main}>
+          {Object.keys(headers).map((val, index) => (
+            <div className={styles.card} key={index}>
+              {val + ": "}
+              <div className={styles.gauge} style={{"--percent": data[0][val]/headers[val] * 100 ?? 0}}>
+                {data[0][val] + "/" + headers[val] ?? 0} 
+              </div>
+            </div>
+          ))}
+        </div>
+      </body>
     </main>
   )
 }
